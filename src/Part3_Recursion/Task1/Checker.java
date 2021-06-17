6070bc9f8ab32962d49cedd7fa3daacc896c8cfe
Densam2704 <den.samoshckin@yandex.ru>
2021-06-17 14:53:58 +0300

    P3T1: added random double array generation

--- src/Part3_Recursion/Task1/Checker.java
+++ src/Part3_Recursion/Task1/Checker.java
@@ -2,26 +2,16 @@ package Part3_Recursion.Task1;
 
 public class Checker {
   public static final int NOT_FOUND = -1;
-  //  public Checker(){}
-//  private int[] array;
-//
-//  public Checker(int[] array) {
-//	this.array = array;
-//  }
-//
-//  public int[] getArray() {
-//	return array;
-//  }
-//
-//  public void setArray(int[] array) {
-//	this.array = array;
-//  }
   
-  public static int binarySearch(int[] array, int key) {
+  public static int binarySearch(double[] array, double key) {
 	return NOT_FOUND;
   }
   
-  public static int bruteForce(int[] array, int key) {
+  public static int binarySearch(double[]array,double key,int startIndex,int endIndex){
+	return NOT_FOUND;
+  }
+  
+  public static int bruteForce(double[] array, double key) {
 	for (int i = 0; i < array.length; i++) {
 	  if (array[i] == key) {
 		return i;
--- src/Part3_Recursion/Task1/Task1.java
+++ src/Part3_Recursion/Task1/Task1.java
@@ -7,13 +7,18 @@ package Part3_Recursion.Task1;
 //Сравните время выполнения обоих решений для больших массивов (например, 100000000 элементов).
 public class Task1 {
   private int[] ints = {0, 1, 2, 2, 1, 3, 4, 9, 10, 11, -1, 3, 2, 15};
+  private double[] doubles = {0.0, 0.1, -0.3, 0.2};
   
   public Task1() {
-	
-	System.out.println(intArrayToString(generateRandomIntArray(10, 10, 1)));
+	int arrayLength = 1000000000;
+	int range = 1000;
+	int offset = -500;
+	System.out.println("Creating array of " + arrayLength + " doubles for comparing binary search and brute force search");
+	doubles=generateRandomDoubleArray(arrayLength,range,offset);
+	ints=generateRandomIntArray(arrayLength,range,offset);
   }
   
-//  Returns random integer array with given length.
+  //  Returns random integer array with given length.
 //  Random numbers are calculated according to following formula:
 //  randomValue = randomFrom (0,range) + offset.
   private static int[] generateRandomIntArray(int length, int range, int offset) {
@@ -25,8 +30,30 @@ public class Task1 {
 	return array;
   }
   
-//  Converts array of integers to one string.
+  //  Converts array of integers to one string.
   private static String intArrayToString(int[] array) {
+	String result = "";
+	for (int i = 0; i < array.length; i++) {
+	  result += array[i] + " ";
+	}
+	result += "\n";
+	return result;
+  }
+  
+  //  Returns random double array with given length.
+//  Random numbers are calculated according to following formula:
+//  randomValue = randomFrom (0,range) + offset.
+  private static double[] generateRandomDoubleArray(int length, double range, double offset) {
+	if (length <= 0) return null;
+	double[] array = new double[length];
+	for (int i = 0; i < length; i++) {
+	  array[i] = (double) (Math.random() * range) + offset;
+	}
+	return array;
+  }
+  
+  //  Converts array of doubles to one string.
+  private static String doubleArrayToString(double[] array) {
 	String result = "";
 	for (int i = 0; i < array.length; i++) {
 	  result += array[i] + " ";
