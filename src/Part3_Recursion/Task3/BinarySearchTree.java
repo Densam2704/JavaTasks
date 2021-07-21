eb751d5d2e9c5d12a64d5d743d475ae2207f5a21
Densam2704 <den.samoshckin@yandex.ru>
2021-07-21 11:25:24 +0300

    P3T3: made function private insert() for recursion.

--- src/Part3_Recursion/Task3/BinarySearchTree.java
+++ src/Part3_Recursion/Task3/BinarySearchTree.java
@@ -2,7 +2,7 @@ package Part3_Recursion.Task3;
 
 public interface BinarySearchTree {
   public Object find(int key);
-  public void insert(int key, Object value);
+  public void insert(int key, Comparable value);
   public void remove(int key);
   public void infixTraverse();
   public void prefixTraverse();
--- src/Part3_Recursion/Task3/Tree.java
+++ src/Part3_Recursion/Task3/Tree.java
@@ -4,20 +4,52 @@ import java.util.HashSet;
 
 public class Tree implements BinarySearchTree{
   private Node root = null;
+  private Node current = null;
   
-  public Tree(){
-    root.parent=root;
-    root.left=root;
-    root.right=root;
+  public Tree(Comparable DataType){
+   root = new Node(DataType);
   }
   @Override
   public Object find(int key) {
 	return null;
   }
   
+//  Дано: дерево Т и пара (K, V).
+//
+//Задача: вставить пару (K, V) в дерево Т (при совпадении K, заменить V).
+//
+//Алгоритм:
+//
+//Если дерево пусто, заменить его на дерево с одним корневым узлом ((K, V), null, null) и остановиться.
+//Иначе сравнить K с ключом корневого узла X.
+//Если K>X, рекурсивно добавить (K, V) в правое поддерево Т.
+//Если K<X, рекурсивно добавить (K, V) в левое поддерево Т.
+//Если K=X, заменить V текущего узла новым значением.
   @Override
-  public void insert(int key, Object value) {
-	
+  public void insert(int key, Comparable value) {
+    current = root;
+//
+	current = insert(current, value);
+  }
+  
+  private Node insert (Node current, Comparable value){
+	if (current==null){
+	  return new Node(value);
+	}
+	else {
+//	  value>current
+	  if (current.data.compareTo(value)<0){
+	    insert(current.right,value);
+	  }
+//	  value<current
+	  else
+	  if (current.data.compareTo(value)>0){
+		insert(current.left,value);
+	  }
+	  else
+	   return current;
+	}
+	return current;
   }
   
   @Override
